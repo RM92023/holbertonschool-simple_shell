50770ce1c2dd8d9e8a8c02c24063ef4043057aa1
sandro132 <mianveo@hotmail.com>
2023-04-20 10:48:19 -0500

    :memo: creating .gitignore and modifying README.md and main.c

--- main.c
+++ main.c
@@ -6,40 +6,51 @@
 
 #define MAX_CMD_LEN 100
 
+/**
+ * main - main command line interface for running
+ *
+ * Return: void
+ */
 int main(void)
 {
-    char cmd[MAX_CMD_LEN];
-    pid_t pid;
-    int status;
+char cmd[MAX_CMD_LEN];
+pid_t pid;
+int status;
 
-    while (1) {
-        printf("$");
-        fflush(stdout);
+while (1)
+{
+printf("$");
+fflush(stdout);
 
-        if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL) {
-            printf("\n");
-            break;
-        }
+if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL)
+{
+printf("\n");
+break;
+}
 
-        if (cmd[strlen(cmd) - 1] == '\n') {
-            cmd[strlen(cmd) - 1] = '\0';
-        }
+if (cmd[strlen(cmd) - 1] == '\n')
+{
+cmd[strlen(cmd) - 1] = '\0';
+}
 
-        pid = fork();
+pid = fork();
 
-        if (pid == -1) {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            execlp(cmd, cmd, (char *) NULL);
-            perror("exec");
-            exit(EXIT_FAILURE);
-        } else {
-            do {
-                waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-    }
-
-    return 0;
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+execlp(cmd, cmd, (char *) NULL);
+perror("exec");
+exit(EXIT_FAILURE);
+}
+else
+{
+do {
+waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
+} return 0;
 }
