f644b9a47aa124270b8d01bea37b850168eb1e45
RM92023 <robinjara20@gmail.com>
2023-04-20 10:09:32 -0500

    Adding main.c - AUTHORS and MANUAL SHELL

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_CMD_LEN 100
+
+int main(void)
+{
+    char cmd[MAX_CMD_LEN];
+    pid_t pid;
+    int status;
+
+    while (1) {
+        printf("$");
+        fflush(stdout);
+
+        if (fgets(cmd, MAX_CMD_LEN, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        if (cmd[strlen(cmd) - 1] == '\n') {
+            cmd[strlen(cmd) - 1] = '\0';
+        }
+
+        pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            execlp(cmd, cmd, (char *) NULL);
+            perror("exec");
+            exit(EXIT_FAILURE);
+        } else {
+            do {
+                waitpid(pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
+
+    return 0;
+}
