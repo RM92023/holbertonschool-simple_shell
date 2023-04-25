9fddd2a0174fc904afda7bd7f2e97f754086496a
RM92023 <robinjara20@gmail.com>
2023-04-24 22:52:19 -0500

    Modifing Prompt

--- prompt.c
+++ prompt.c
@@ -1,38 +1,56 @@
 #include "main.h"
 
 #define MAX_COMMAND 10
+#define MAX_PATH_LENGTH 1024
 
 void prompt(char **av, char **env)
 {
     char *str = NULL;
+    char *dir;
+    int found = 0;
     int i, j, status;
     size_t n = 0;
     ssize_t num_char;
     char *argv[MAX_COMMAND];
-    char *path, *cmd_path, *token;
-    int line_num = 1;
-
     pid_t pid;
+    char *path = getenv("PATH");
+    char *directories[MAX_COMMAND];
+    int num_directories = 0;
+    char path_copy[MAX_PATH_LENGTH];
+    char cwd[MAX_PATH_LENGTH];
+    (void)av;
 
-    /* Agregar path del directorio actual al comienzo de PATH */
-    char *current_dir = getcwd(NULL, 0);
-    char *new_path = malloc(strlen(current_dir) + strlen(getenv("PATH")) + 2);
-    sprintf(new_path, "%s:%s", current_dir, getenv("PATH"));
-    setenv("PATH", new_path, 1);
-    free(current_dir);
-    free(new_path);
+    /* Parse PATH variable */
+    dir = strtok(strcpy(path_copy, path), ":");
+    while (dir != NULL)
+    {
+        directories[num_directories] = dir;
+        num_directories++;
+        dir = strtok(NULL, ":");
+    }
 
     while (1)
     {
+        char path_buffer[MAX_PATH_LENGTH];
+        if (getcwd(cwd, MAX_PATH_LENGTH) == NULL)
+        {
+            fprintf(stderr, "Error obteniendo el directorio actual: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
         if (isatty(STDIN_FILENO))
         {
-            printf("$ ");
+            printf("%s$ ", cwd);
         }
         num_char = getline(&str, &n, stdin);
         if (num_char == -1)
         {
             free(str);
             exit(EXIT_SUCCESS);
+        }
+        if (num_char == 1)
+        {
+            /* Línea vacía */
+            continue;
         }
         i = 0;
         while (str[i])
@@ -43,8 +61,6 @@ void prompt(char **av, char **env)
             }
             i++;
         }
-
-        path = getenv("PATH");
         j = 0;
         argv[j] = strtok(str, " ");
         while (argv[j] != NULL)
@@ -52,50 +68,59 @@ void prompt(char **av, char **env)
             j++;
             argv[j] = strtok(NULL, " ");
         }
-        argv[j] = NULL; /* Asegurarse de que el último elemento sea NULL. */
+
+        /* Handle built-in commands */
+        if (strcmp(argv[0], "cd") == 0)
+        {
+            if (argv[1] == NULL)
+            {
+                /* Change to home directory */
+                chdir(getenv("HOME"));
+            }
+            else if (chdir(argv[1]) != 0)
+            {
+                printf("%s: No such file or directory\n", argv[1]);
+            }
+            continue;
+        }
+        else if (strcmp(argv[0], "exit") == 0)
+        {
+            exit(0);
+        }
+
+        /* Check if command exists in PATH */
+        found = 0;
+        for (i = 0; i < num_directories; i++)
+        {
+            snprintf(path_buffer, MAX_PATH_LENGTH, "%s/%s", directories[i], argv[0]);
+            if (access(path_buffer, X_OK) == 0)
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+        {
+            printf("%s: command not found\n", argv[0]);
+            continue;
+        }
+
+        /* Fork and execute command */
         pid = fork();
         if (pid == -1)
         {
-            free(str);
+            fprintf(stderr, "Error al crear un proceso hijo: %s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
         if (pid == 0)
         {
-            if ((argv[0] == NULL) || strlen(argv[0]) == 0)
-            {
-                continue;
-            }
-
-            if (execve(argv[0], argv, env) == -1)
-            { 
-                token = strtok(path, ":");
-                while (token != NULL)
-                {
-                    cmd_path = malloc(strlen(token) + strlen(argv[0]) + 2);
-                    sprintf(cmd_path, "%s/%s", token, argv[0]);
-                    if (access(cmd_path, F_OK) == 0)
-                    {
-                        argv[0] = cmd_path;
-                        execve(argv [0], argv, env);
-                    }
-                    else
-                    {
-                        free(cmd_path);
-                        token =strtok(NULL, ":");
-                    }
-                }
-                fprintf(stderr, "%s: %d: %s: not found\n", av[0], line_num, argv[0]);
-                free(str);
-                exit(127); /* Comando no encontrado */
-            }
-            else
-            {
-                return;
-            }
+            execve(path_buffer, argv, env);
+            fprintf(stderr, "Error al ejecutar el comando %s: %s\n", argv[0], strerror(errno));
+            exit(EXIT_FAILURE);
         }
         else
         {
-            wait(&status);
+            waitpid(pid, &status, 0);
         }
     }
 }
