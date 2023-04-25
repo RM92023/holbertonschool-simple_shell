405fb630021ddc9e458fba3cf4329ace0414dd76
RM92023 <robinjara20@gmail.com>
2023-04-25 08:16:30 -0500

    MOdify prompt and gitignore

--- prompt.c
+++ prompt.c
@@ -1,116 +1,257 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // For strtok() and strcmp()
+#include <unistd.h> // For fork(), pid_t
+#include <sys/wait.h> // For waitpid() and associated macros
 
-#define MAX_COMMAND 10
+char SHELL_NAME[50] = "myShell";
+int QUIT = 0;
 
-extern char **environ;
-
-void prompt(char **av, char **env)
+// Function to read a line from command into the buffer
+char *readLine()
 {
-    char *str = NULL;
-    char *path;
-    int i, j, status;
-    int found;
-    size_t n = 0;
-    ssize_t num_char;
-    char *argv[MAX_COMMAND];
-    pid_t pid;
+	char *line = (char *)malloc(sizeof(char) * 1024); // Dynamically Allocate Buffer
+	char c;
+	int pos = 0, bufsize = 1024;
+	if (!line) // Buffer Allocation Failed
+	{
+		printf("\nBuffer Allocation Error.");
+		exit(EXIT_FAILURE);
+	}
+	while(1)
+	{
+		c=getchar();
+		if (c == EOF || c == '\n') // If End of File or New line, replace with Null character
+		{
+			line[pos] = '\0';
+			return line;
+		}
+		else
+		{
+			line[pos] = c;
+		}
+		pos ++;
+		// If we have exceeded the buffer
+		if (pos >= bufsize)
+		{
+			bufsize += 1024;
+			line = realloc(line, sizeof(char) * bufsize);
+			if (!line) // Buffer Allocation Failed
+			{
+			printf("\nBuffer Allocation Error.");
+			exit(EXIT_FAILURE);
+			}
+		}
+	}
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            printf("%s$ ", av[0]);
-        }
+}
 
-        num_char = getline(&str, &n, stdin);
+// Function to split a line into constituent commands
+char **splitLine(char *line)
+{
+	char **tokens = (char **)malloc(sizeof(char *) * 64);
+	char *token;
+	char delim[10] = " \t\n\r\a";
+	int pos = 0, bufsize = 64;
+	if (!tokens)
+	{
+		printf("\nBuffer Allocation Error.");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, delim);
+	while (token != NULL)
+	{
+		tokens[pos] = token;
+		pos ++;
+		if (pos >= bufsize)
+		{
+			bufsize += 64;
+			line = realloc(line, bufsize * sizeof(char *));
+			if (!line) // Buffer Allocation Failed
+			{
+			printf("\nBuffer Allocation Error.");
+			exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, delim);
+	}
+	tokens[pos] = NULL;
+	return tokens;
+}
 
-        if (num_char == -1)
-        {
-            free(str);
-            exit(EXIT_SUCCESS);
-        }
+// Section Dealing with Built-in Commands
 
-        i = 0;
-        while (str[i])
-        {
-            if (str[i] == '\n')
-            {
-                str[i] = 0;
-            }
-            i++;
-        }
+// Function Declarations
+int myShell_cd(char **args);
+int myShell_exit();
 
-        j = 0;
-        argv[j] = strtok(str, " ");
+// Definitions
+char *builtin_cmd[] = {"cd", "exit"};
 
-        while (argv[j] != NULL)
-        {
-            argv[++j] = strtok(NULL, " ");
-        }
+int (*builtin_func[]) (char **) = {&myShell_cd, &myShell_exit}; // Array of function pointers for call from execShell
 
-        if ((argv[0] == NULL) || strlen(argv[0]) == 0)
-        {
-            continue;
-        }
+int numBuiltin() // Function to return number of builtin commands
+{
+	return sizeof(builtin_cmd)/sizeof(char *);
+}
 
-        path = NULL;
-        found = 0;
+// Builtin command definitions
+int myShell_cd(char **args)
+{
+	if (args[1] == NULL) 
+	{
+		printf("myShell: expected argument to \"cd\"\n");
+	} 
+	else 
+	{
+		if (chdir(args[1]) != 0) 
+		{
+			perror("myShell: ");
+		}
+	}
+	return 1;
+}
 
-        /* Check if the command is in the PATH */
-        if (strchr(argv[0], '/') == NULL)
-        {
-            char *token = strtok(env[0], ":");
+int myShell_exit()
+{
+	QUIT = 1;
+	return 0;
+}
 
-            while (token != NULL)
-            {
-                path = malloc(strlen(token) + strlen(argv[0]) + 2);
-                sprintf(path, "%s/%s", token, argv[0]);
+// Function to create child process and run command
+int myShellLaunch(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+	pid = fork();
+	if (pid == 0)
+	{
+		// The Child Process
+		if (execvp(args[0], args) == -1)
+		{
+			perror("myShell: ");
+		}
+	exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		//Forking Error
+		perror("myShell: ");
+	}
+	else
+	{
+		// The Parent Process
+	do 
+	{
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return 1;
+}
 
-                if (access(path, X_OK) == 0)
-                {
-                    found = 1;
-                    break;
-                }
+// Function to execute command from terminal
+int execShell(char **args)
+{
+	int ret;
+	if (args[0] == NULL)
+	{
+		// Empty command
+		return 1;
+	}
+	// Loop to check for builtin functions
+	for (int i=0; i< numBuiltin(); i++) // numBuiltin() returns the number of builtin functions
+	{
+		if(strcmp(args[0], builtin_cmd[i])==0) // Check if user function matches builtin function name
+			return (*builtin_func[i])(args); // Call respective builtin function with arguments
+	}
+	ret = myShellLaunch(args);
+	return ret;
+}
 
-                free(path);
-                path = NULL;
-                token = strtok(NULL, ":");
-            }
-        }
-        else if (access(argv[0], X_OK) == 0) /* Command is an absolute path */
-        {
-            found = 1;
-            path = argv[0];
-        }
+// Read and Parse from Config File
+int readConfig()
+{
+	FILE *fptr;
+	char line[200];
+	char **args;
+	fptr = fopen("config", "r");
+	if (fptr == NULL)
+	{
+		printf("Unable to find config file.\n");
+		return 1;
+	}
+	else
+	{
+		while(fgets(line, sizeof(line), fptr) != NULL)
+		{
+			printf("\n%s", line);
+			args=splitLine(line);
+			if(strcmp(args[0], "export")==0)
+				strcpy(SHELL_NAME, args[1]);
+		}
+	}
+	free(args);
+	fclose(fptr);
+	return 1;
+}
 
-        if (!found)
-        {
-            printf("%s: command not found\n", argv[0]);
-            continue;
-        }
+// When myShell is called Interactively
+int myShellInteract()
+{
+	char *line;
+	char **args;
+	while(QUIT == 0)
+	{
+		printf("%s> ", SHELL_NAME);
+		line=readLine();
+		args=splitLine(line);
+		execShell(args);
+		free(line);
+		free(args);
+	}
+	return 1;
+}
 
-        pid = fork();
+// When myShell is called with a Script as Argument
+int myShellScript(char filename[100])
+{
+	printf("Received Script. Opening %s", filename);
+	FILE *fptr;
+	char line[200];
+	char **args;
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		printf("\nUnable to open file.");
+		return 1;
+	}
+	else
+	{
+		printf("\nFile Opened. Parsing. Parsed commands displayed first.");
+		while(fgets(line, sizeof(line), fptr)!= NULL)
+		{
+			printf("\n%s", line);
+			args=splitLine(line);
+			execShell(args);
+		}
+	}
+	free(args);
+	fclose(fptr);
+	return 1;
+}
 
-        if (pid == -1)
-        {
-            free(str);
-            exit(EXIT_FAILURE);
-        }
+int main(int argc, char **argv)
+{
+	// Read from myShell Configuration Files
+	readConfig();
+	// Parsing commands Interactive mode or Script Mode
+	if (argc == 1)
+		myShellInteract();
+	else if (argc == 2)
+		myShellScript(argv[1]);
+	else
+		printf("\nInvalid Number of Arguments");
 
-        if (pid == 0)
-        {
-            if (execve(path, argv, environ) == -1)
-            {
-                printf("%s: command not found\n", argv[0]);
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-
-        free(path);
-        path = NULL;
-    }
+	// Exit the Shell
+	return EXIT_SUCCESS;
 }
