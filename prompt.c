bb870ec9cef8d23fbec9e4c7022c266350380b53
RM92023 <robinjara20@gmail.com>
2023-04-26 23:01:10 -0500

    modify prompt because is necesary two validations

--- prompt.c
+++ prompt.c
@@ -9,16 +9,15 @@ void prompt(char **av __attribute__((unused)), char **env)
     size_t n = 0;
     ssize_t num_char;
     char *argv[MAX_COMMAND];
-    char *path, *cmd_path, *token;
-    char **ptr;/*exit and env*/
-    /*bool found; exit and env*/
+    char *path, *cmd_path, *token, *path1;
+    char **ptr;
     pid_t pid;
     while (1)
     {
         if (isatty(STDIN_FILENO))
         {
-        printf("$ ");
-        fflush(stdout); /*Add this line to ensure that the prompt is printed on the screen*/
+            printf("$ ");
+            fflush(stdout);
         }
         num_char = getline(&str, &n, stdin);
         if (num_char == -1)
@@ -26,7 +25,7 @@ void prompt(char **av __attribute__((unused)), char **env)
             free(str);
             exit(last_cmd_exit_status);
         }
-               i = 0;
+        i = 0;
         while (str[i])
         {
             if (str[i] == '\n')
@@ -37,7 +36,6 @@ void prompt(char **av __attribute__((unused)), char **env)
         }
 
         path = getenv("PATH");
-        /*found = false; new line env exit*/
         j = 0;
         argv[j] = strtok(str, " ");
         while (argv[j] != NULL)
@@ -45,44 +43,28 @@ void prompt(char **av __attribute__((unused)), char **env)
             argv[++j] = strtok(NULL, " ");
         }
 
-        /*begin exit and env*/
         if (strcmp(argv[0], "clear") == 0)
-            {
-                system("clear");
-                continue;
-            }
-
-        if (strcmp(argv[0], "exit") == 0)
-            {
-                free(str);
-                exit(last_cmd_exit_status);
-            }
-        /*Check if the command is "cd"*/
-        if (strcmp(argv[0], "cd") == 0)
         {
-            if (argv[1] == NULL) /*If no path is specified, change to home directory*/
-            {
-                chdir(getenv("HOME"));
-            }
-            else
-            {
-                chdir(argv[1]);
-            }
+            system("clear");
             continue;
         }
 
+        if (strcmp(argv[0], "exit") == 0)
+        {
+            free(str);
+            exit(last_cmd_exit_status);
+        }
+
         if (strcmp(argv[0], "env") == 0)
-            {
-                ptr = env;
-                while (*ptr != NULL)
+        {
+            ptr = env;
+            while (*ptr != NULL)
             {
                 printf("%s\n", *ptr);
                 ptr++;
             }
             continue;
         }
-        /*End Exit and env*/
-
 
         pid = fork();
         if (pid == -1)
@@ -98,54 +80,58 @@ void prompt(char **av __attribute__((unused)), char **env)
             }
 
             if (execve(argv[0], argv, env) == -1)
-            { 
-                token = strtok(path, ":");
-                while (token != NULL)
+            {
+                if (path != NULL)
                 {
-                    cmd_path = malloc(strlen(token) + strlen(argv[0]) + 2);
-                    sprintf(cmd_path, "%s/%s", token, argv[0]);
+                    token = strtok(path, ":");
+                    while (token != NULL)
+                    {
+                        cmd_path = malloc(strlen(token) + strlen(argv[0]) + 2);
+                        sprintf(cmd_path, "%s/%s", token, argv[0]);
+                        if (access(cmd_path, F_OK) == 0)
+                        {
+                            argv[0] = cmd_path;
+                            execve(argv [0], argv, env);
+                        }
+                        else
+                        {
+                            free(cmd_path);
+                            token = strtok(NULL, ":");
+                        }
+                    }
+                }
+
+                /* Check if PATH1 exists */
+                path1 = getenv("PATH1");
+                if (path1 != NULL)
+                {
+                    /* Try to find the command in PATH1 */
+                    cmd_path = malloc(strlen(path1) + strlen(argv[0]) + 2);
+                    sprintf(cmd_path, "%s/%s", path1, argv[0]);
                     if (access(cmd_path, F_OK) == 0)
                     {
                         argv[0] = cmd_path;
-                        execve(argv [0], argv, env);
+                        execve(argv[0], argv, env);
                     }
                     else
                     {
                         free(cmd_path);
-                        token =strtok(NULL, ":");
                     }
                 }
-                    fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-                    free(str);
-                    exit(127); /*Change 127 to failure*/
 
-                /*fprintf(stderr, "%s:command not found\n", av[0]);
+                /* Print an error message if the command is not found */
+                fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
                 free(str);
-                exit(127);*//*Change FAILURE to SUCCESS*/
+                exit(127);
             }
-            /*else if (execve(argv[0], argv, env) == 0)
-            {
-                fprintf(stderr, "%s: 1: %s: not found\n", av[0], argv[0]);
-                free(str);
-                exit(EXIT_SUCCESS);
-            }*/
-            /*else
-            {
-                return;
-            }*/
         }
         else
         {
-            wait(&status);
-            last_cmd_exit_status = WEXITSTATUS(status);
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                last_cmd_exit_status = WEXITSTATUS(status);
+            }
         }
-    }
-    if (strcmp(argv[0], "exit") == 0)
-    {
-        exit(2);
-    }
-    else
-    {
-        exit(0);
     }
 }
