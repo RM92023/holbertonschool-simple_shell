c6d0e278d21cdb19d6fd083a41da983637220019
RM92023 <robinjara20@gmail.com>
2023-04-27 11:34:53 -0500

    Updating style betty

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
+/**
+ * main - main function
+ *
+ * @ac: application context
+ * @av: application context
+ * @env: application environment
+ *
+ * Return: application context
+ */
 
 int main(int ac, char **av, char **env)
 {
-    if (ac == 1)
-    {
-        prompt(av, env);
-    }
-    return (0);
+if (ac == 1)
+{
+prompt(av, env);
+}
+return (0);
 }
--- prompt.c
+++ prompt.c
@@ -2,117 +2,120 @@
 
 #define MAX_COMMAND 10
 
+/**
+ * prompt - prompt for command line arguments
+ *
+ * @av: command line arguments
+ * @env: environment
+ *
+ * Return: command line arguments
+ */
+
 void prompt(char **av __attribute__((unused)), char **env)
 {
-    char *str = NULL;
-    int i, j, status, last_cmd_exit_status = 0;
-    size_t n = 0;
-    ssize_t num_char;
-    char *argv[MAX_COMMAND];
-    char *path, *cmd_path, *token;
-    char **ptr;
-    pid_t pid;
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            printf("$ ");
-            fflush(stdout);
-        }
-        num_char = getline(&str, &n, stdin);
-        if (num_char == -1)
-        {
-            free(str);
-            exit (WEXITSTATUS(status));
-        }
-        i = 0;
-        while (str[i])
-        {
-            if (str[i] == '\n')
-            {
-                str[i] = 0;
-            }
-            i++;
-        }
-
-        if (strcmp(argv[0], "clear") == 0)
-        {
-            system("clear");
-            continue;
-        }
-
-        if (strcmp(argv[0], "exit") == 0)
-        {
-            free(str);
-            exit(last_cmd_exit_status);
-        }
-
-        if (strcmp(argv[0], "env") == 0)
-        {
-            ptr = env;
-            while (*ptr != NULL)
-            {
-                printf("%s\n", *ptr);
-                ptr++;
-            }
-            continue;
-        }
-
-        pid = fork();
-        if (pid == -1)
-        {
-            free(str);
-            exit(1);
-        }
-        if (pid == 0)
-        {
-            path = getenv("PATH");
-            j = 0;
-            argv[j] = strtok(str, " ");
-            while (argv[j])
-            {
-                argv[++j] = strtok(NULL, " ");
-            }
-            
-            if ((argv[0] == NULL) || strlen(argv[0]) == 0)
-            {
-                continue;
-            }
-
-            if (execve(argv[0], argv, env) == -1)
-            {
-                if (path != NULL)
-                {
-                    token = strtok(path, ":");
-                    while (token != NULL)
-                    {
-                        cmd_path = malloc(strlen(token) + strlen(argv[0]) + 2);
-                        sprintf(cmd_path, "%s/%s", token, argv[0]);
-                        if (access(cmd_path, F_OK) == 0)
-                        {
-                            argv[0] = cmd_path;
-                            execve(argv [0], argv, env);
-                        }
-                        else
-                        {
-                            free(cmd_path);
-                            token = strtok(NULL, ":");
-                        }
-                    }
-                }
-                /* Print an error message if the command is not found */
-                fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-                free(str);
-                exit(127);
-            }
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status))
-            {
-                last_cmd_exit_status = WEXITSTATUS(status);
-            }
-        }
-    }
+char *str = NULL;
+int i, j, status, last_cmd_exit_status = 0;
+size_t n = 0;
+ssize_t num_char;
+char *argv[MAX_COMMAND];
+char *path, *cmd_path, *token;
+char **ptr;
+pid_t pid;
+while (1)
+{
+if (isatty(STDIN_FILENO))
+{
+printf("$ ");
+fflush(stdout);
+}
+num_char = getline(&str, &n, stdin);
+if (num_char == -1)
+{
+free(str);
+exit(WEXITSTATUS(status));
+}
+i = 0;
+while (str[i])
+{
+if (str[i] == '\n')
+{
+str[i] = 0;
+}
+i++;
+}
+path = getenv("PATH");
+j = 0;
+argv[j] = strtok(str, " ");
+while (argv[j])
+{
+argv[++j] = strtok(NULL, " ");
+}
+if (strcmp(argv[0], "clear") == 0)
+{
+system("clear");
+continue;
+}
+if (strcmp(argv[0], "exit") == 0)
+{
+free(str);
+exit(last_cmd_exit_status);
+}
+if (strcmp(argv[0], "env") == 0)
+{
+ptr = env;
+while (*ptr != NULL)
+{
+printf("%s\n", *ptr);
+ptr++;
+}
+continue;
+}
+pid = fork();
+if (pid == -1)
+{
+free(str);
+exit(1);
+}
+if (pid == 0)
+{
+if ((argv[0] == NULL) || strlen(argv[0]) == 0)
+{
+continue;
+}
+if (execve(argv[0], argv, env) == -1)
+{
+if (path != NULL)
+{
+token = strtok(path, ":");
+while (token != NULL)
+{
+cmd_path = malloc(strlen(token) + strlen(argv[0]) + 2);
+sprintf(cmd_path, "%s/%s", token, argv[0]);
+if (access(cmd_path, F_OK) == 0)
+{
+argv[0] = cmd_path;
+execve(argv [0], argv, env);
+}
+else
+{
+free(cmd_path);
+token = strtok(NULL, ":");
+}
+}
+}
+/* Print an error message if the command is not found */
+fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+free(str);
+exit(127);
+}
+}
+else
+{
+waitpid(pid, &status, 0);
+if (WIFEXITED(status))
+{
+last_cmd_exit_status = WEXITSTATUS(status);
+}
+}
+}
 }
